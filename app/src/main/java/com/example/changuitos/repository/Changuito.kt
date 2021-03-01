cbe12016034eedbcb115428fc470470ec9053909
kilia <kiliagonias@gmail.com>
2021-03-01 21:22:33 +0100

    initial commit

---
+++ app/src/main/java/com/example/changuitos/repository/Changuito.kt
@@ -0,0 +1,11 @@
+package com.example.changuitos.repository
+data class Changuito(
+    val id:Int,
+    val nombre:String,
+    val raza:String,
+    val fotoResid:Int,
+    val sexo:String,
+    val edad: String,
+    val descripcion:String
+
+)
